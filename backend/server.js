86397fce8300e6d823bffc6f7a703f9484748749
Piyush Mishra <pm9958478949@gmail.com>
2025-09-11 00:02:02 +0530

    API variable changed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const downloaderRoutes = require("./routes/downloader");
+const downloaderRoutes = require("../");
 
 const app = express();
 const PORT = 8000;
